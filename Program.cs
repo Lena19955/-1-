5e2534a8dfaaf163b3fd7b1d068efd1fdcd0ab64
Lena <Lenysik1995@example.com>
2023-01-12 13:55:11 +0500

    write method formationNewArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string [] array = new string [6] {"SQL", "C++", "Java", "Python", "C#", "Hello"};
 string [] newArray = new string [array.Length];
+
+void formationNewArray(string [] array, string [] newArray)
+{
+   int count = 0;
+   for (int i = 0; i <= array.Length; i++)
+   {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+   }
+}
